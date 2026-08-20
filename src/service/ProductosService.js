acc1981df9cc5406bf856fc9790d7625d0c20b27
DonMatii <contacto.donmatii@gmail.com>
2026-08-19 21:26:37 -0400

    feat: integración exitosa de Google OAuth y ajuste en ProductosService para enviar JWT

--- src/service/ProductosService.js
+++ src/service/ProductosService.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 
-// ¡Ahora apuntamos a nuestro propio servidor backend en Spring Boot!
+// Apuntamos a nuestro servidor backend en Spring Boot
 const URL_BASE = "http://localhost:8080";
 
 export const obtenerProductos = async () => {
   try {
-    // Aquí es donde realmente se "llama" a la API
-    const response = await axios.get(`${URL_BASE}/api/productos`);
+    // 1. Rescatamos el token sagrado que guardamos al iniciar sesión con Google
+    const token = sessionStorage.getItem("userToken");
+
+    // 2. Configuramos los encabezados (Headers) para mostrarle el "carnet" al guardia de Java
+    const headers = token ? { Authorization: `Bearer ${token}` } : {};
+
+    // 3. Hacemos la petición a la API enviando los headers de seguridad
+    const response = await axios.get(`${URL_BASE}/api/productos`, { headers });
     
-    // Devolvemos solo los datos que nos interesan
+    // Devolvemos los datos del catálogo
     return response.data; 
   } catch (error) {
     console.error("Error al conectar con la API:", error);
